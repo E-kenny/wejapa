37ec94103ced45583f4387d0f224c5345e2c50f1
Ekene Obiasogu <ekennyobiasogu@gmail.com>
2020-08-09 17:25:20 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<?php
+ $color=$_POST["color"];
+ $dateofbirth=$_POST["date"];
+ $firstname=$_POST["firstname"];
+ $secondname=$_POST["secondname"];
+ $email=$_POST["email"];
+ $gender=$_POST["gender"];
+ $department=$_POST["department"];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <style>
+        body{
+            background-color:<?php echo $color ;?>;
+        }
+        .grid{
+            display:grid;
+            grid-template-columns:20% 25% 25% 20% ;
+            grid-template-rows:auto auto;
+            justify-content:space-between;
+            align-content:center;
+        }
+        .grid-item1{
+            grid-column:2/4;
+    
+        }
+        .grid-item2{
+            grid-row:2/3;
+    
+        }
+       .grid-item4{
+        grid-area:2/3/3/4 ;
+       } 
+       .grid-item3{
+        grid-area:2/2/3/3 ;
+       } 
+       h1{
+           text-align:center;
+       }
+
+    </style>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body >
+<h1>my web page</h1>
+    <div class="grid">
+       <div class="grid-item1"><h3>wejapa Internship week 3 task (form validation)</h3></div>
+       <div class="grid-item2">My name is <?php echo $firstname." ". $secondname ; ?> I was born <?php echo $dateofbirth;?> I am a <?php echo $gender ;?> from Nigeria and I'm single, in the department of <?php echo $department;?> and my email address is <?php echo $email ; ?> </div>
+       <div class="grid-item3">I'm a mechanical Engineering graduate from formally Anambra state university now Chukwuemeka Odumegwu Ojukwu university.</div>
+       <div class="grid-item4">I love coding, sport(soccer), mathematics, cars and technology.</div>
+    </div>
+</body>
+</html>
